24485e40a4696d3275ce5ca420e469710ee0f711
karyu83 <java_2@shChae>
2016-04-18 16:19:31 +0900

    GameDb매퍼 추가 / root-context.xml에서 데이터소스 계정 변경 scott.sql로 oracle에 nexven 계정 생성 밑 테스트용 테이블 생성 가능

---
+++ src/main/java/com/unknown/mapper/GameDbMapper.java
@@ -0,0 +1,15 @@
+package com.unknown.mapper;
+
+
+import java.util.List;
+
+import com.unknown.model.Dept;
+import com.unknown.model.GameDb;
+
+public interface GameDbMapper {
+	List<GameDb> selectAll();
+	GameDb selectBygNum(int gNum);
+	int insert(GameDb gameDb);
+	int updateBygNum(GameDb gameDb);
+	int deleteBygNum(int gNum);
+}
---
+++ src/main/java/com/unknown/service/GameDbService.java
@@ -0,0 +1,49 @@
+package com.unknown.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unknown.mapper.GameDbMapper;
+import com.unknown.model.Dept;
+import com.unknown.model.GameDb;
+
+@Service
+public class GameDbService {
+	//필드주입
+	@Autowired
+	GameDbMapper gameDbMapper;
+	
+	@Transactional
+	public List<GameDb> getlist(){
+		
+		List<GameDb> games = gameDbMapper.selectAll();
+		
+		return games;
+	}
+	@Transactional
+	public GameDb getGameDb(Integer gNum){
+		return gameDbMapper.selectBygNum(gNum);
+	}
+	
+	@Transactional
+	public void insert(GameDb gameDb){
+		gameDbMapper.insert(gameDb);
+	}
+	
+	@Transactional
+	public void update(GameDb gameDb){
+		gameDbMapper.updateBygNum(gameDb);
+	}
+	
+	@Transactional
+	public GameDb delete(Integer gNum){
+		GameDb gameDb = gameDbMapper.selectBygNum(gNum);
+		gameDbMapper.deleteBygNum(gNum);
+		return gameDb;		
+	}
+	
+	
+}
